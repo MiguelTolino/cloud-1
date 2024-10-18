fe2cdac877e3c2d85994ed109a0fdcc1bca6e37d
MiguelTolino <mmateo-t@student.42madrid.com>
2024-10-19 01:41:44 +0200

    Add startup script to install dependencies and clone Inception repository

---
+++ startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update package list and install packages
+sudo apt update
+sudo apt install -y make docker.io docker-compose
+
+# Agregar el usuario actual al grupo docker
+echo "Añadiendo el usuario $(whoami) al grupo 'docker'..."
+usermod -aG docker $USER
+
+# Reiniciar el servicio Docker
+echo "Reiniciando el servicio Docker..."
+sudo systemctl restart docker
+
+# Aplicar el grupo 'docker' sin cerrar sesión
+echo "Aplicando el nuevo grupo 'docker' para el usuario actual..."
+sudo newgrp docker
+
+# Clonar el repositorio de Inception
+git clone https://github.com/MiguelTolino/inception.git
+cd inception
+
+# Add .env inside the inception folder
+# TODO: Options: scp | google secret manager
+
+make all
